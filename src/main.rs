82163961588b809983466db116d9dfc074334f6c
filipton <filipton12@gmail.com>
2024-09-03 15:45:04 +0200

    feat: test udp quic server (with sni parsing)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::net::UdpSocket;
+
 use aes::cipher::{BlockEncrypt, KeyInit};
 use aes_gcm::{aead::AeadMutInPlace, Aes128Gcm};
 use anyhow::Result;
@@ -91,7 +93,65 @@ pub fn parse_quic_frame(data: &[u8]) -> Option<QuicFrameData> {
 }
 
 fn main() -> Result<()> {
-    let payload = hex::decode(std::fs::read_to_string("./encrypted-packet.txt")?.trim())?;
-    println!("{:02X?}", parse_quic_frame(&payload));
-    Ok(())
+    let listener = UdpSocket::bind("0.0.0.0:8443")?;
+
+    let mut recv_buf = [0; 65536];
+    loop {
+        let (n, addr) = listener.recv_from(&mut recv_buf)?;
+        println!("recv: {n}bytes from: {addr}");
+
+        let quic_frame = parse_quic_frame(&recv_buf[..n]);
+        if let Some(quic_frame) = quic_frame {
+            if quic_frame.frame_type == 6 {
+                println!("INITIAL FRAME: {quic_frame:02X?}");
+                let sni_res = parse_sni_inner(&quic_frame.decoded_frame);
+                println!("{sni_res:?}");
+            }
+        }
+    }
+}
+
+// from fkm-proxy
+pub fn parse_sni_inner(buf: &[u8]) -> Option<String> {
+    let handshake_type = *buf.get(0)?; // 1byte
+    if handshake_type != 1 {
+        return None;
+    }
+
+    let session_id_length = *buf.get(38)? as usize; // 1byte
+    let cipher_suites_len: u16 = u16::from_be_bytes([
+        *buf.get(39 + session_id_length + 0)?,
+        *buf.get(39 + session_id_length + 1)?,
+    ]); // 2bytes
+
+    let mut offset: usize = 39 + session_id_length + 2 + cipher_suites_len as usize;
+    let compression_methods_len = *buf.get(offset)? as usize; // 1byte
+    offset += 1 + compression_methods_len;
+
+    let mut extensions_len: u16 = u16::from_be_bytes([*buf.get(offset)?, *buf.get(offset + 1)?]); // 2bytes
+    offset += 2;
+
+    while extensions_len > 0 {
+        let ext_type: u16 = u16::from_be_bytes([*buf.get(offset)?, *buf.get(offset + 1)?]);
+        let ext_len: u16 = u16::from_be_bytes([*buf.get(offset + 2)?, *buf.get(offset + 3)?]);
+        offset += 4;
+
+        if ext_type == 0 {
+            let server_name_type = *buf.get(offset + 2)?;
+            let server_name_length: u16 =
+                u16::from_be_bytes([*buf.get(offset + 3)?, *buf.get(offset + 4)?]);
+
+            let server_name = &buf.get((offset + 5)..(offset + 5 + server_name_length as usize))?;
+            let server_name = core::str::from_utf8(server_name).ok()?;
+
+            if server_name_type == 0 {
+                return Some(server_name.to_string());
+            }
+        }
+
+        offset += ext_len as usize;
+        extensions_len -= 4 + ext_len;
+    }
+
+    None
 }
